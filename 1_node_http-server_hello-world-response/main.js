66d56644dacc3165a7d9f1ff0e553ec3f3d5204c
qoolixiloop <qoolixiloop@gmx.ch>
2019-03-28 23:32:16 +0100

    changes in main.js

--- 1_node_http-server_hello-world-response/main.js
+++ 1_node_http-server_hello-world-response/main.js
@@ -1,13 +1,25 @@
 var http = require("http");
+var url = require('url');
 
 http.createServer(function (request, response) {
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
+  // Send the HTTP header 
+  // HTTP Status: 200 : OK
+  // Content Type: text/plain
+  response.writeHead(200, {'Content-Type': 'text/plain'});
    
-   // Send the response body as "Hello World"
-   response.end('Hello World\n');
+  // Read URL
+  var query = url.parse(request.url, true).query;
+  var text = query.year + " " + query.month;
+  console.log("text= " + text);
+  
+  // Check URL
+  if (text!=="") {
+    // Send the response body of the text variable
+    response.end(text+"\n");
+  }else{
+    // Send the default response body as "Hello World"
+    response.end('Hello World\n');
+  }
 }).listen(8081);
 
 // Console will print the message
--- 5_npm_build-tool-/assets/scripts/main.js
+++ 5_npm_build-tool-/assets/scripts/main.js
@@ -1,21 +1,26 @@
 (function(){
   "use strict";
 
+  //Constructor Author
   var Author = function(name){
     this.name = name || "Anonymous";
     this.articles = [];
   };
 
+  //member function
   Author.prototype.writeArticle = function(title){
     this.articles.push(title);
   };
 
+  //member function
   Author.prototype.listArticles = function(){
     return this.name + " has written: " + this.articles.join(", ");
   };
 
+  //make available
   exports.Author = Author;
 
+  //create an object
   var peter = new Author("Peter");
   peter.writeArticle("A Beginners Guide to npm");
   peter.writeArticle("Using npm as a build tool");
